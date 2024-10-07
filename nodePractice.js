bc12ee65cc034d958c05564ce740719f83addc18
kreit <reitenbaughkm@gmail.com>
2024-10-07 10:27:57 -0500

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   nodePractice.js

--- nodePractice.js
+++ nodePractice.js
@@ -1,4 +1,7 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
-const getInput = require("./get-input.js");
+const getInput = require("./get-input.js"){
+    return process.argv[n + 1]
+}
+
 const greet = "Hello"
-console
+console.log("Hello" + getInput)
