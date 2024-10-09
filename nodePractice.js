cb66228dad133784fa8cec37fd6da7af4e47cd4c
kreit <reitenbaughkm@gmail.com>
2024-10-09 06:12:43 -0500

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   nodePractice.js

--- nodePractice.js
+++ nodePractice.js
@@ -1,7 +1,21 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js")
 
-const firstName = getInput(1);
-const lastName = getInput(2)
+const firstName = getInput(1);  
+const lastName = getInput(2);
 
-console.log('Hello + ')
+console.log(`Hello ${firstName} ${lastName}`);
+
+console.log(`${fullNameCapitalized} is your name capitalized.`)
+
+const fullNameCapitalized = `${getInput(1)} ${getInput(2)}`;
+
+const initials = "KyleRose"
+
+console.log(initials[0])
+
+console.log(initials[4])
+
+const email = `${getInput(1)}${getInput(2)}.prsvr@gmail.com`;
+
+console.log(`Your persevere.com email is: ${email}`);
